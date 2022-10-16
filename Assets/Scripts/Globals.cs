ed92218084692f3b5a14bcd1b2303b3d2d7c8380
iubaldo <ianzachary.u@gmail.com>
2022-10-15 19:53:58 -0500

    added sounds to melee ego

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,4 +4,6 @@ public static class Globals
     public enum floorType { history, technology, literature, art }
 
     public static floorType selectedFloor = floorType.history;
+
+    public static int currentMoney = 0;
 }
